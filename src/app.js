4372bdaee2dd90ecd36b442e21ecfd62658559f5
JorgeCoto98 <120097112+JorgeCoto98@users.noreply.github.com>
2023-07-25 01:03:38 +0000

    Entrega

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = Math.floor(Math.random() * 12) + 1;
+  let numberDom = document.getElementById("numero");
+  numberDom.innerText = num;
+
+  var figuras = ["♦", "♥", "♠", "♣"];
+  var figura = figuras[Math.floor(Math.random() * 4)];
+
+  // Colocar el icono en la esquina superior izquierda
+  let iconoDom = document.getElementById("icono");
+  iconoDom.innerText = figura;
+
+  // Establecer el color de las figuras en los bordes
+  if (figura === "♥" || figura === "♦") {
+    iconoDom.style.color = "red";
+    numberDom.style.color = "red";
+  } else {
+    iconoDom.style.color = "black";
+    numberDom.style.color = "black";
+  }
+  //Copia del icono transversalmente
+
+  let iconoCopiaDom = document.createElement("div");
+  iconoCopiaDom.classList.add("icono-copia");
+  iconoCopiaDom.innerText = figura;
+  iconoCopiaDom.style.color =
+    figura === "♥" || figura === "♦" ? "red" : "black";
+
+  let cartaDiv = document.getElementById("carta");
+  cartaDiv.appendChild(iconoCopiaDom);
 };
